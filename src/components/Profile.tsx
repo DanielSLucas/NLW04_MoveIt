eec881dcc30aee1e897e5c2a12e92ce6751cd5e2
Daniel Lucas <daniellucas-pms@hotmail.com>
2021-03-01 11:02:59 -0300

    NLW dia 04

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/55390286?s=460&u=cadd24b56ac02812055ae6c8745600954953c847&v=4" alt="Daniel Lucas"/>
@@ -9,7 +13,7 @@ export function Profile() {
         <strong>Daniel Lucas</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
